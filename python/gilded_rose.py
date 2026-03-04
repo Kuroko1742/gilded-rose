9cf4e13b28568da7dbd8a257a56d686717a562a2
Kuroko1742 <nathan.vazille17@gmail.com>
2026-03-04 09:42:45 +0100

    Sécuriser quality

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -18,6 +18,8 @@ class GildedRose(object):
                 if item.quality > 0:
                     if not is_sulfuras:
                         item.quality = item.quality - (2 if is_conjured else 1)
+                        if item.quality < 0:
+                            item.quality = 0
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -36,6 +38,8 @@ class GildedRose(object):
                         if item.quality > 0:
                             if not is_sulfuras:
                                 item.quality = item.quality - (2 if is_conjured else 1)
+                                if item.quality < 0:
+                                    item.quality = 0
                     else:
                         item.quality = item.quality - item.quality
                 else:
